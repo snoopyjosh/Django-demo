ef05b2317da1fc365f1ef6c74d48bc71b4a53e12
josh <snoopyjsc@gmail.com>
2022-10-05 20:51:31 +0800

    Django-password

--- generator/views.py
+++ generator/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def password(request):
+
+    #a~z
+    chars=[ chr(c) for c in range(97,123)]
+    print(chars)
+
+    input_length=request.GET.get('input_length')
+    print(input_length)
+
     return render(request,'./password.html',{'password':123456})
 
 def index(request):
