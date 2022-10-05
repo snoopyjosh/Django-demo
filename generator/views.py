21e931db8298a7d6a836d35191128dc0d68be610
josh <snoopyjsc@gmail.com>
2022-10-05 20:38:07 +0800

    Django-password

---
+++ generator/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def password(request):
+    return render(request,'./password.html',{'password':123456})
+
+def index(request):
+    print('Hello Django')
+   
+    return render(request,'./index.html')
